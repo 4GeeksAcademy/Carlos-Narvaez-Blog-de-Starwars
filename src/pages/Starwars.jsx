d6b58fbb99f77abefb82871a1f82610eb1c9073c
Carlos Narváez <narvaezcarlos88@gmail.com>
2026-04-20 02:21:36 +0000

    updates

---
+++ src/pages/Starwars.jsx
@@ -0,0 +1,43 @@
+// Import necessary components from react-router-dom and other parts of the application.
+import { Await, Link } from "react-router-dom";
+import useGlobalReducer from "../hooks/useGlobalReducer";  // Custom hook for accessing the global state.
+import { CardPeople } from "../components/CardPeople.jsx";
+
+export const Starwars = () => {
+  // Access the global state and dispatch function using the useGlobalReducer hook.
+ const { store, dispatch } = useGlobalReducer()
+
+ async function cartaPersonajes() {
+    try {
+        const response = await fetch("https://www.swapi.tech/api/people");
+        if(!response.ok){
+          throw new Error("Error al obtener el personaje: ${response.statusText}")
+        }
+
+        const data await response.json()
+        
+      
+    } catch (error) {
+        console.error("Error en cargar personajes", error)
+    } 
+ }
+    fetch("https://www.swapi.tech/api/people")
+        .then(res => res.json())
+        .then(data => console.log(data.results))
+        .catch(err => console.error(err))
+
+  return (
+    <div className="container">
+        <h2>Starwars</h2>
+        <h3>People</h3>
+            <CardPeople/>
+        <h3>Planets</h3>
+        {/*componente_carta_planets*/}
+        <h3>Vehicles</h3>
+        {/*componente_carta_vehicles*/}
+      <Link to="/">
+        <button className="btn btn-primary">Back home</button>
+      </Link>
+    </div>
+  );
+};
